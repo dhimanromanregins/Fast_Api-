00172a1c7129929757ed74e4fc1246c650403665
dhimanromanregins <52999468+dhimanromanregins@users.noreply.github.com>
2022-11-03 17:37:38 +0530

    fastapi

--- main.py
+++ main.py
@@ -4,8 +4,20 @@ app = FastAPI()
 
 @app.get('/')
 def index():
-    return {'data':{'name':'sahil'}}
+    return {'data':{'name':'Blog-list'}}
 
-@app.get('/about')
-def about():
-    return {'data':{'data':'about-page'}}
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':{'data':'Unpublished Blogs'}}
+
+@app.get('/blog/{id}')
+def about(id:int):
+    # fetch blog with id = id
+    return {'data':{'data':id}}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id:int):
+    return {'data':{'1', '2'}}
+
+
